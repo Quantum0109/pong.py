729af9ee5b06677f4198b8d6dd7d644d6d0b8e18
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2022-03-12 13:45:52 +0300

    Add files via upload

--- pygame/main.py
+++ pygame/main.py
@@ -6,13 +6,23 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("windows 10")
 
 all_sprites = pygame.sprite.Group()
-player = Player()
+player = Player(screen)
 all_sprites.add(player)
 
 clock = pygame.time.Clock()
 
 while True:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        elif event.type == pygame.KEYDOWN:             
+            if event.key == pygame.K_DOWN:    
+                player.rect.y += 10
+            elif event.key == pygame.K_UP:
+                player.rect.y -= 10
+                    
     all_sprites.update()
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(400)
--- pygame/player.py
+++ pygame/player.py
@@ -1,6 +1,13 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen):
         super().__init__()
-        
+        self.image = pygame.Surface((25, 150))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = screen // 2
+
+    def update(self):
+        pass
+
